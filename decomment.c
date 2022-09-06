955fca15f426e3cb4b38c3c55028f34a479f2ec4
veronicaglh <veronicagt.IT@gmail.com>
2022-09-06 09:51:07 +0300

    first commit

---
+++ decomment.c
@@ -0,0 +1,3 @@
+/*This program will decomment the user input using c language*/
+/*For example let us say user inputs the following text: abc/*defghi the output will just be abc*/
+/*The program will also decomment other types of user inputs*/
