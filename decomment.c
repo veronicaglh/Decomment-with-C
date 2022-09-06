79fb387633328bfeae659c734adfb04d10e97f25
veronicaglh <veronicagt.IT@gmail.com>
2022-09-06 09:54:01 +0300

    store all the states of the DFA

--- decomment.c
+++ decomment.c
@@ -1,3 +1,13 @@
 /*This program will decomment the user input using c language*/
 /*For example let us say user inputs the following text: abc/*defghi the output will just be abc*/
 /*The program will also decomment other types of user inputs*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Let us first make an enum to store all the possible states of the DFA*/
+enum States
+{
+    NORMAL, SLASH, STARBG, STARED, DQUOTE, SQUOTE, BSLASH, DQ_BSLASH, SQ_BSLASH, S_BSLASH 
+};
